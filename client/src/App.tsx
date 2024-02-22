35a97c7242ba61ebfbd5078847bb9a546651366c
Shahwaiz Zahid <shahwaizmughal940@gmail.com>
2024-02-22 10:43:25 -0800

    update show password component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Home from './Home'
-import {Routes, Route, useLocation } from "react-router-dom"
-import SignUp from './SignUp'
-import Login from './Login'
-
-
+import React from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { Routes, Route, useLocation } from "react-router-dom";
+import SignUp from "./SignUp";
+import Login from "./Login";
 
 export default function App() {
-  const { pathname } = useLocation()
-  
+  const { pathname } = useLocation();
+
   return (
-   <>
-  {pathname === "/" && (
-<Navbar />     
-   )}
-  <Routes>
-    <Route path='/' element={<Home />}/>
-    <Route path="/signup" element={<SignUp />} />
-    <Route path="/login" element={<Login />} />
-  </Routes>
-   </>
-  )
+    <>
+      {pathname === "/" && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
