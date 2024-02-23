45a60f5daad38c355db8daa19f2275b9d6a0552f
fexxix <fexxix@gmail.com>
2024-02-23 06:32:13 +0500

    add: added token model and modifed user model accordingly

--- server/models/usersModel.ts
+++ server/models/usersModel.ts
@@ -1,17 +1,18 @@
 import { Schema, model } from "mongoose"
 
 const usersSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
   email: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
   },
 })
 
