395de50fc8ada76c67e613c9c0182df6b5039c5c
fexxix <fexxix@gmail.com>
2024-02-23 06:33:09 +0500

    add: modified /signup route to send verification email on success

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,29 +1,73 @@
-import { compare, hash } from "bcrypt"
-import { Router } from "express"
+import { compare, genSalt, hash } from "bcrypt"
+import { Router, type Request } from "express"
 import { usersModel } from "../models/usersModel"
+import { tokenModel } from "../models/tokenModel"
+import { sendVerificationEmail } from "../controllers/sendEmail"
+import crypto from "crypto"
+
+interface SignUpRequest extends Request {
+  body: {
+    email: string
+    password: string
+    username: string
+  }
+}
 
 const usersRouter = Router()
 
-usersRouter.post("/signup", async (req, res) => {
+usersRouter.post("/signup", async (req: SignUpRequest, res) => {
   const { username, email, password } = req.body
 
   const exists = await usersModel.findOne({ email })
+
+  if (exists && !exists.verified) {
+    await exists.deleteOne()
+
+    return res
+      .status(400)
+      .json({
+        message:
+          "Email already in use but not verified! Submit again to verify.",
+      })
+  }
 
   if (exists) {
     return res.status(400).json({ message: "Email already in use!" })
   }
 
-  const hashedPassword = await hash(password, 10)
+  const salt = await genSalt(10)
+  const hashedPassword = await hash(password, salt)
+
+  const user = new usersModel({
+    email,
+    username,
+    password: hashedPassword,
+  })
 
   try {
-    const user = await usersModel.create({
-      username,
-      email,
-      password: hashedPassword,
-    })
-    res.status(200).json(user)
-  } catch (e: any) {
-    res.status(400).json({ message: e.message })
+    await user.save()
+  } catch {
+    return res.status(500).json({ message: "Error while creating user!" })
+  }
+
+  const tokenDoc = new tokenModel({
+    userId: user._id,
+    token: crypto.randomBytes(32).toString("hex"),
+  })
+
+  try {
+    await tokenDoc.save()
+  } catch {
+    return res.status(500).json({ message: "Error while creating token!" })
+  }
+
+  try {
+    await sendVerificationEmail({ email, token: tokenDoc.token })
+    res
+      .status(200)
+      .json({ message: "Signup successful! Verification email sent!" })
+  } catch {
+    return res.status(500).json({ message: "Error while sending email!" })
   }
 })
 
