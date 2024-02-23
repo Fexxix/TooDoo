988ee80902ae2cfa38cc2ab4e8e875370109dcef
fexxix <fexxix@gmail.com>
2024-02-23 06:31:18 +0500

    add: email template and sending function

---
+++ server/controllers/email.tsx
@@ -0,0 +1,16 @@
+import { Html } from "@react-email/html"
+import { Link } from "@react-email/link"
+
+export function Email({ token }: { token: string }) {
+  return (
+    <Html lang="en">
+      <h1>Verify Your email</h1>
+      Click this:{" "}
+      <Link href={`http://${process.env.FRONTEND_URL}/verify/${token}`}>
+        Verify
+      </Link>
+      <br />
+      This link will expire in an hour.
+    </Html>
+  )
+}
---
+++ server/controllers/sendEmail.ts
@@ -0,0 +1,45 @@
+import { createTransport } from "nodemailer"
+import { render } from "@react-email/render"
+import { Email } from "./email"
+
+const transporter = createTransport({
+  host: process.env.SMTP_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: process.env.SMTP_USER,
+    clientId: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    accessToken: process.env.OAUTH_ACCESS_TOKEN,
+    expires: Number(process.env.OAUTH_TOKEN_EXPIRES),
+  },
+})
+
+export async function sendVerificationEmail({
+  email,
+  token,
+}: {
+  email: string
+  token: string
+}) {
+  const emailHTML = render(Email({ token }))
+
+  console.log("Email HTML:", emailHTML)
+
+  const message = {
+    from: process.env.SMTP_USER,
+    to: email,
+    subject: "Verify your email",
+    html: emailHTML,
+  }
+
+  try {
+    await transporter.sendMail(message)
+    console.log("Email sent")
+  } catch (error) {
+    console.error("Error sending email:", error)
+    throw error
+  }
+}
