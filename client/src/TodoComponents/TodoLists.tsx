d9378e19bfef6bff461aba1409c027d613c16192
fexxix <fexxix@gmail.com>
2024-02-25 18:46:24 +0500

    chore: added start script for server

--- client/src/TodoComponents/TodoLists.tsx
+++ client/src/TodoComponents/TodoLists.tsx
@@ -1,67 +1,63 @@
-import React from 'react'
-
-
-export default function TodooLists(props:any) {
-  const {setTodosList , todoList} =props
+export default function TodooLists(props: any) {
+  const { setTodosList, todoList } = props
 
   const handleComplete = (i: number) => {
-    setTodosList((p:any) => {
-      const updatedTodos = [...p];
+    setTodosList((p: any) => {
+      const updatedTodos = [...p]
       if (updatedTodos[i].editImage === "check-solid.svg") {
         updatedTodos[i] = {
           ...updatedTodos[i],
           editImage: "pen-to-square-solid.svg",
-        };
-        updatedTodos[i] = { ...updatedTodos[i], inputType: true };
-        updatedTodos[i] = { ...updatedTodos[i], border: "none" };
-        updatedTodos[i] = { ...updatedTodos[i], isComplete: true };
+        }
+        updatedTodos[i] = { ...updatedTodos[i], inputType: true }
+        updatedTodos[i] = { ...updatedTodos[i], border: "none" }
+        updatedTodos[i] = { ...updatedTodos[i], isComplete: true }
       }
       updatedTodos[i] = {
         ...updatedTodos[i],
         complete: "check-double-solid.svg",
-      };
-      updatedTodos[i] = { ...updatedTodos[i], textDecor: "line-through" };
-      return updatedTodos;
-    });
-  };
+      }
+      updatedTodos[i] = { ...updatedTodos[i], textDecor: "line-through" }
+      return updatedTodos
+    })
+  }
 
   const handleDel = (i: number) => {
-    setTodosList([...todoList.slice(0, i), ...todoList.slice(i + 1)]);
-  };
+    setTodosList([...todoList.slice(0, i), ...todoList.slice(i + 1)])
+  }
 
   const handleEdit = (i: number) => {
-    if(!todoList[i]){
+    if (!todoList[i]) {
       if (todoList[i].editImage === "pen-to-square-solid.svg") {
-        setTodosList((prevTodos:any) => {
-          const updatedTodos = [...prevTodos];
-          updatedTodos[i] = { ...updatedTodos[i], editImage: "check-solid.svg" };
-          updatedTodos[i] = { ...updatedTodos[i], inputType: false };
-          updatedTodos[i] = { ...updatedTodos[i], border: "2" };
-          return updatedTodos;
-        });
+        setTodosList((prevTodos: any) => {
+          const updatedTodos = [...prevTodos]
+          updatedTodos[i] = { ...updatedTodos[i], editImage: "check-solid.svg" }
+          updatedTodos[i] = { ...updatedTodos[i], inputType: false }
+          updatedTodos[i] = { ...updatedTodos[i], border: "2" }
+          return updatedTodos
+        })
       } else {
-        setTodosList((prevTodos:any) => {
-          const updatedTodos = [...prevTodos];
+        setTodosList((prevTodos: any) => {
+          const updatedTodos = [...prevTodos]
           updatedTodos[i] = {
             ...updatedTodos[i],
             editImage: "pen-to-square-solid.svg",
-          };
-          updatedTodos[i] = { ...updatedTodos[i], inputType: true };
-          updatedTodos[i] = { ...updatedTodos[i], border: "none" };
-          return updatedTodos;
-        });
+          }
+          updatedTodos[i] = { ...updatedTodos[i], inputType: true }
+          updatedTodos[i] = { ...updatedTodos[i], border: "none" }
+          return updatedTodos
+        })
       }
     }
-
-  };
+  }
 
   const handleInputChange = (index: number, newText: string) => {
-    setTodosList((prevTodos:any) => {
-      const updatedTodos = [...prevTodos];
-      updatedTodos[index].text = newText;
-      return updatedTodos;
-    });
-  };
+    setTodosList((prevTodos: any) => {
+      const updatedTodos = [...prevTodos]
+      updatedTodos[index].text = newText
+      return updatedTodos
+    })
+  }
 
   return (
     <>
@@ -74,25 +70,26 @@ export default function TodooLists(props:any) {
           alignItems: "center",
         }}
       >
-        <div onClick={()=>{setTodosList([])}} className="cursor-pointer my-4 text-white relative left-0 w-fit h-fit  py-3 px-4 bg-orange-400 rounded-full hover:bg-orange-600 transform hover:scale-110 hover:scale-x-125 transition-transform duration-300 ease-in-out ">
+        <div
+          onClick={() => {
+            setTodosList([])
+          }}
+          className="cursor-pointer my-4 text-white relative left-0 w-fit h-fit  py-3 px-4 bg-orange-400 rounded-full hover:bg-orange-600 transform hover:scale-110 hover:scale-x-125 transition-transform duration-300 ease-in-out "
+        >
           clear all
         </div>
-        {todoList.map((todo:any, index:any) => (
+        {todoList.map((todo: any, index: any) => (
           <div
             key={index}
             className="w-[450px] mt-3 h-14 flex bg-orange-400 justify-center item-center px-2 rounded-2xl"
           >
             <div
               onClick={() => {
-                handleComplete(index);
+                handleComplete(index)
               }}
               className="w-6 h-full item-center mx-2"
             >
-              <img
-                className="w-full h-full "
-                src={todo.complete}
-                alt=""
-              />
+              <img className="w-full h-full " src={todo.complete} alt="" />
             </div>
             <input
               onChange={(e) => handleInputChange(index, e.target.value)}
@@ -103,7 +100,7 @@ export default function TodooLists(props:any) {
             />
             <div
               onClick={() => {
-                handleEdit(index);
+                handleEdit(index)
               }}
               className="w-6 h-full ml-6  mx-2"
             >
@@ -115,7 +112,7 @@ export default function TodooLists(props:any) {
             </div>
             <div
               onClick={() => {
-                handleDel(index);
+                handleDel(index)
               }}
               className="w-6 h-full mx-2"
             >
