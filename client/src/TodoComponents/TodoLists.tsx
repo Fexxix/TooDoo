97200a9f5fc2d97bbdddd6ea96f02124698335bc
Shahwaiz Zahid <shahwaizmughal940@gmail.com>
2024-03-05 00:20:46 -0800

    Add updates in todo

--- client/src/TodoComponents/TodoLists.tsx
+++ client/src/TodoComponents/TodoLists.tsx
@@ -1,7 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 import { useTodo } from "../Context/TodoProvider";
-
-export default function TodoLists() {
+ export default function TodoLists() {
   const {
     todos,
     todostyle,
@@ -9,18 +8,16 @@ export default function TodoLists() {
     edit,
     handleInputChange,
     handleTextAreaChange,
-    description,
+    description
   } = useTodo()!;
-
   return (
     <>
       {todos.map((todo: any, index: number) => (
-        <div
-          key={uuidv4()}
-          className="bg-orange-400 my-2 lg:w-[65%] md:w-[80%] w-[90%] rounded-2xl overflow-hidden"
-        >
-          <div className="bg-orange-400 h-fit text-white flex flex-col px-6 py-3 justify-center items-center">
-            <input
+       <div key={index}
+       className="bg-orange-400 my-2 lg:w-[65%] md:w-[80%] w-[90%] rounded-2xl overflow-hidden"
+     >
+       <div className="bg-orange-400 h-fit text-white flex flex-col px-6 py-3 justify-center items-center">
+       <input
               onChange={(e) => {
                 handleInputChange(e, index);
               }}
@@ -33,8 +30,8 @@ export default function TodoLists() {
               value={todostyle[index]?.title}
             />
 
-            <div className="w-full h-full flex flex-col justify-center items-center mt-2">
-              <div
+         <div className="w-full h-full flex flex-col justify-center items-center mt-2">
+         <div
                 className="w-5 h-5"
                 onClick={() => {
                   description(index);
@@ -50,6 +47,7 @@ export default function TodoLists() {
                 />
               </div>
 
+           
               <textarea
                 onChange={(e) => {
                   handleTextAreaChange(e, index);
@@ -63,15 +61,13 @@ export default function TodoLists() {
                 value={todostyle[index]?.des} // Added empty string as fallback value
                 style={{
                   maxHeight: !todostyle[index]?.description ? "0px" : "100px",
-                  borderColor: !todostyle[index]?.description
-                    ? "#ED8936"
-                    : "white",
+                  borderColor :!todostyle[index]?.description ? "#ED8936" : "white",
                 }}
               ></textarea>
-            </div>
-          </div>
+         </div>
+       </div>
 
-          <div className="w-full  h-fit flex justify-center items-center  text-white text-lg font-bold ">
+       <div className="w-full  h-fit flex justify-center items-center  text-white text-lg font-bold ">
             <button
               onClick={() => {
                 edit(todo.id, index);
@@ -90,7 +86,46 @@ export default function TodoLists() {
               Remove
             </button>
           </div>
-        </div>
+     </div>
+    // <div  className="bg-orange-400 my-2 lg:w-[65%] md:w-[80%] w-[90%] rounded-2xl overflow-hidden">
+    //       <div className="bg-orange-400 h-fit text-white flex flex-col px-6 py-3 justify-center items-center">
+    //         <input
+    //           className={`text-2xl bg-orange-400 border-white py-1 px-3 w-full outline-none my-1 rounded-xl`}
+    //           type="text"
+    //           value={"bjbjbjb"}
+    //         />
+
+    //         <div className="w-full h-full flex flex-col justify-center items-center mt-2">
+    //           <div
+    //             className="w-5 h-5"
+    //             onClick={() => {
+    //               setDescription(des ? false : true);
+    //             }}
+    //           >
+    //             <img src={"angle-down-solid.svg"} alt="" />
+    //           </div>
+
+    //           <textarea
+    //             className={`bg-orange-400 scrollbar-hide px-3 py-1 rounded-lg mt-2 resize-none text-lg text-white  border-white  outline-none w-full transition-max-height duration-300`}
+    //             id={`description`} // Added index to make id unique
+    //             disabled={des}
+    //             rows={2}
+    //             value={"bjjjjb"} // Added empty string as fallback value
+    //             style={{
+    //               maxHeight: des ? "0px" : "100px",
+    //             }}
+    //           ></textarea>
+    //         </div>
+    //       </div>
+
+    //       <div className="w-full  h-fit flex justify-center items-center  text-white text-lg font-bold ">
+    //         <button className="overflow border-black border-2 mx-2 my-1 bg-black px-6 py-2 rounded-2xl"></button>
+
+    //         <button className="overflow border-black border-2 mx-2 my-1 bg-black px-6 py-2 rounded-2xl">
+    //           Remove
+    //         </button>
+    //       </div>
+    //     </div>
       ))}
     </>
   );
--- client/src/TodoComponents/TodoNavbar.tsx
+++ client/src/TodoComponents/TodoNavbar.tsx
@@ -4,7 +4,7 @@ function TodoNavbar() {
   const { logout } = useAuth()!;
   return (
     <>
-      <div className="bg-orange-400 fixed w-full top-0  h-fit items-center flex justify-between z-50 px-6">
+      <div className="bg-orange-400  w-full  h-fit items-center flex justify-between z-50 px-6">
         <h1 className="text-white  w-fit text-4xl font-bold py-3 ">
           My Toodoos
         </h1>
